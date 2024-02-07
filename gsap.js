0cc0f633019008840ae5f6a96a6fe79dc891b27c
lyoshyy <153962615+lyoshyy@users.noreply.github.com>
2024-02-07 04:48:20 +0300

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -7,8 +7,8 @@ gsap.matchMedia().add("(min-width: 992px)", () => {
       scrollTrigger: {
         trigger: ".section_h-header",
         // trigger element - viewport
-        start: "0% 20%",
-        end: "0% -20%",
+        start: "200% 20%",
+        end: "200% -20%",
         scrub: 1,
         markers: false,
       },
